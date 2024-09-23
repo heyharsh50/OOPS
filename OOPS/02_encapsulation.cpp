bff80c97e55cc0b7d299760add9749ef4fe1be84
Harsh Raj <harshraj88253@gmail.com>
2024-09-23 19:49:22 +0000

    encapsulation and data hiding

---
+++ OOPS/02_encapsulation.cpp
@@ -0,0 +1,22 @@
+/* Encapsulation is wrapping up of data and member function in a single unit called class.*/
+
+//Example:-
+class Account {
+private:
+    double balance;
+    string password;    // data hiding
+
+public:
+    //properties | attributes
+    string accountId;
+    string username;
+    
+    //methods | member function
+
+};
+
+//Encapsulation helps in data hiding.
+//Data Hiding -> it means hide the sensitive information. How? -> (by making private access modifier or Protected).
+
+/* So, Encapsulation is basically creating a class and 
+   inside the class declaring the data/properties + member function. */

e097956e954ddb3fdc389e8113c059387600fe29
Harsh Raj <harshraj88253@gmail.com>
2024-10-19 13:48:00 +0530

    Operator overloading

---
+++ OOPS/12_operator-overloading.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+
+class Add{
+    int value;
+    
+    public:
+        Add(int v = 0) : value(v) {}
+        
+        // Overloading the '+' operator
+        Add operator+(const Add& obj) {
+            Add temp;
+            temp.value = this->value + obj.value;
+            return temp;
+        }
+        
+        void display() {
+            cout << "Value = " << value << endl;
+        }
+};
+
+int main(){
+
+    Add a1(10), a2(20);
+    Add a3 = a1 + a2;  // This calls the overloaded '+' operator
+    a3.display();      // Output: Value = 30
+    
+    return 0;
+}
